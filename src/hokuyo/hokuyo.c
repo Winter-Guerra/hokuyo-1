e10fc379ed6b475403a6981d1bded9d901b3edfd
Winter Guerra <winterg@mit.edu>
2016-07-06 19:14:20 -0400

    Got the LIDAR node to take in data from the laser scanner.
    Now need to marshal that data to LCM.

--- src/hokuyo/hokuyo.c
+++ src/hokuyo/hokuyo.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <math.h>
 #include <getopt.h>
@@ -24,6 +25,45 @@
 #include "liburg/include/urg_sensor.h"
 #include "liburg/samples/open_urg_sensor.h"
 
+// Useful for printing the scan data.
+static void print_data(urg_t *urg, long data[], int data_n, long time_stamp)
+{
+#if 1
+    int front_index;
+
+    (void)data_n;
+
+    // \~japanese �O���̃f�[�^�݂̂��\��
+    front_index = urg_step2index(urg, 0);
+    printf("Distance from front of LIDAR is %ld [mm] @ timestamp %ld [msec].\n", data[front_index], time_stamp);
+
+#else
+    (void)time_stamp;
+
+    int i;
+    long min_distance;
+    long max_distance;
+
+    // \~japanese �S�Ẵf�[�^�� X-Y �̈ʒu���\��
+    urg_distance_min_max(urg, &min_distance, &max_distance);
+    for (i = 0; i < data_n; ++i) {
+        long l = data[i];
+        double radian;
+        long x;
+        long y;
+
+        if ((l <= min_distance) || (l >= max_distance)) {
+            continue;
+        }
+        radian = urg_index2rad(urg, i);
+        x = (long)(l * cos(radian));
+        y = (long)(l * sin(radian));
+        printf("(%ld, %ld), ", x, y);
+    }
+    printf("\n");
+#endif
+}
+
 int main(int argc, char *argv[])
 {
     setlinebuf(stdout);
@@ -35,16 +75,20 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    // Params from the laser scanner
     urg_t urg;
     int min_step;
     int max_step;
     long min_distance;
     long max_distance;
+    int skip_scan = 9; // This is the max that it can be.
 
+    // Open the laser scanner
     if (open_urg_sensor(&urg, argc, argv) < 0) {
         return 1;
     }
 
+    // Print some information about the laser scanner
     printf("Sensor product type: %s\n", urg_sensor_product_type(&urg));
     printf("Sensor firmware version: %s\n", urg_sensor_firmware_version(&urg));
     printf("Sensor serial ID: %s\n", urg_sensor_serial_id(&urg));
@@ -59,6 +103,20 @@ int main(int argc, char *argv[])
 
     printf("scan interval: %ld [usec]\n", urg_scan_usec(&urg));
     printf("sensor data size: %d\n", urg_max_data_size(&urg));
+
+    // Begin polling the laserscanner for data.
+    bool catastrophicError = !!urg_start_measurement(&urg, URG_DISTANCE, URG_SCAN_INFINITY, skip_scan);
+    long *data = (long*)malloc(urg_max_data_size(&urg) * sizeof(data[0]));
+    long timestamp;
+    // Retrieve Distance data for all eternity
+
+    while (!catastrophicError){
+      int n = urg_get_distance(&urg, data, &timestamp);
+      catastrophicError = (n<0);
+      // Debug
+      print_data(&urg, data, n, timestamp);
+    }
+
 
     urg_close(&urg);
 
